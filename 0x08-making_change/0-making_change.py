e367375a7191784ff5a9299caaffb44dcc6fc496
Kingvadee <ojeifod8@gmail.com>
2024-02-29 23:10:11 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,14 +14,14 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [float('inf') for i in range(total + 1)]
+    table = [sys.maxsize for i in range(total + 1)]
     table[0] = 0
     m = len(coins)
     for i in range(1, total + 1):
         for j in range(m):
             if coins[j] <= i:
                 subres = table[i - coins[j]]
-                if subres != float('inf') and subres + 1 < table[i]:
+                if subres != sys.maxsize and subres + 1 < table[i]:
                     table[i] = subres + 1
 
     if table[total] == sys.maxsize:
