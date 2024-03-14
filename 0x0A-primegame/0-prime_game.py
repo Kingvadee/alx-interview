027c3f3e746f027d14a37b787d7ae29a992b1da8
Kingvadee <ojeifod8@gmail.com>
2024-03-14 10:19:48 -0700

    Ben wins because there are no prime numbers for Maria to choose

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,30 +1,5 @@
 #!/usr/bin/python3
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-def sieve_of_eratosthenes(n):
-    primes = [True] * (n+1)
-    primes[0] = primes[1] = False
-    p = 2
-    while p * p <= n:
-        if primes[p]:
-            for i in range(p * p, n+1, p):
-                primes[i] = False
-        p += 1
-    return [i for i in range(n+1) if primes[i]]
-
 def isWinner(x, nums):
     wins = {"Maria": 0, "Ben": 0}
     for n in nums:
@@ -32,13 +7,15 @@ def isWinner(x, nums):
         maria_turn = True
         while primes:
             if maria_turn:
-                pick = primes.pop(0)
+                pick = max(primes)
+                primes.remove(pick)
                 for i in range(pick, n+1, pick):
                     if i in primes:
                         primes.remove(i)
                 maria_turn = False
             else:
-                pick = primes.pop(0)
+                pick = max(primes)
+                primes.remove(pick)
                 for i in range(pick, n+1, pick):
                     if i in primes:
                         primes.remove(i)
@@ -54,6 +31,15 @@ def isWinner(x, nums):
     else:
         return None
 
-# Test the function
-print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+# Importing the sieve_of_eratosthenes function here
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n+1, p):
+                primes[i] = False
+        p += 1
+    return [i for i in range(n+1) if primes[i]]
 
