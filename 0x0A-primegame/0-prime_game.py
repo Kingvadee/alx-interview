b477df2cd3531491c781ba54886b0d8a6864e875
Kingvadee <ojeifod8@gmail.com>
2024-03-14 10:29:39 -0700

    Ben wins because there are no prime numbers for Maria to choose

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -57,10 +57,11 @@ def isPrime(n):
     if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
         return False
     else:
-        # Not prime if divisable by another number less
+        # Not prime if divisible by another number less
         # or equal to the square root of itself.
         # n**(1/2) returns square root of n
         for i in range(3, int(n**(1/2))+1, 2):
             if n % i == 0:
-                return "Not prime"
+                return False  # Changed this line
         return True
+
